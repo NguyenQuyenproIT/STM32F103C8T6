fddc2206371933935b39fd3599aeb9f1984a6cd5
Quyen Nguyen Van <125990069+NguyenQuyenproIT@users.noreply.github.com>
2025-11-11 16:15:56 +0700

    Update main.c

--- Embedded_System/Big_Excercise_FreeRTOS/main.c
+++ Embedded_System/Big_Excercise_FreeRTOS/main.c
@@ -1,26 +1,21 @@
 #include "stm32f10x.h"                  // Device header
-#include "uart1.h"            // Keil::Device:StdPeriph Drivers:USART
-#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
-#include "stdio.h"
-#include <string.h>
-#include <stdlib.h>	
-//#include "stm32f10x_usart.h"            // Keil::Device:StdPeriph Drivers:USART
-
-#include "stm32f10x_usart.h"
-	
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "uart1.h"
 #include "FreeRTOS.h"                   // ARM.FreeRTOS::RTOS:Core
-#include "FreeRTOSConfig.h"             // ARM.FreeRTOS::RTOS:Config	
+#include "task.h"                       // ARM.FreeRTOS::RTOS:Core
 #include "queue.h"                      // ARM.FreeRTOS::RTOS:Core
-#include "timers.h"                     // ARM.FreeRTOS::RTOS:Timers
+#include <string.h>
+#include <stdio.h>
 
 #define MAX 100
 
-char vrc_Getc;
-char vrc_Res[MAX]; // M?ng luu chu?i nh?p v�o
-int vri_Count = 0;
-int vri_Stt = 0;
+volatile char vrc_Getc;
+volatile char vrc_Res[MAX]; // Mang luu chuoi nhap vào
+volatile int vri_Count = 0;
+volatile int vri_Stt = 0;
 
-void config_led(){ // config for GPIO - PB0
+void Config_LED(){ 
 	GPIO_InitTypeDef gpio_init;
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	
@@ -35,8 +30,6 @@ void config_led(){ // config for GPIO - PB0
 void USART1_IRQHandler() {
     if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET) {
         vrc_Getc = USART_ReceiveData(USART1);
-     //   USART1_SendChar(vrc_Getc); // echo test
-
         if (vrc_Getc == '\r' || vrc_Getc == '\n') {
             if (vri_Count > 0) {
                 vrc_Res[vri_Count] = '\0';
@@ -50,20 +43,106 @@ void USART1_IRQHandler() {
     }
 }
 
-int main(){
-	// SystemInit(); // Khoi tao he thong xung nhip
-	PIN_MODE_UART1();                    // cau h�nh UART
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); // enable ngat RXNE
-//	UART1_Interrupt_Config();            // enable NVIC
-	config_led();                        // config PA0
-	printf("start");
-	while(1) {
-    if(vri_Stt) {
-        vri_Stt = 0;
-        printf(" Received: %s\n", vrc_Res);
-        if(strcmp(vrc_Res, "ON") == 0) GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-        if(strcmp(vrc_Res, "OFF") == 0) GPIO_SetBits(GPIOA, GPIO_Pin_0);
-				if((strcmp(vrc_Res, "TOGGLE") == 0)) GPIOA -> ODR ^= 1;
+QueueHandle_t xQueueCmd;
+QueueHandle_t xQueueFeedback;  // hàng doi goi phan hoi ve UART
+QueueHandle_t xQueueAction;    // hàng doi chia lenh dieu khien GPIO
+
+typedef enum {
+    CMD_NONE = 0,
+    CMD_ON,
+    CMD_OFF,
+    CMD_TOGGLE
+} Command_t;
+
+// -------------------- Task 1: Nh?n chu?i --------------------
+void Task_UART_Receive(void *pvParameters) {
+    char buffer[MAX];
+  //  USART1_SendString("Task_UART_Receive started\r\n");
+
+    while (1) {
+        if (vri_Stt == 1) {
+            vri_Stt = 0;
+            taskENTER_CRITICAL();
+            strcpy(buffer, (const char *)vrc_Res);
+            taskEXIT_CRITICAL();
+            xQueueSend(xQueueCmd, buffer, 0);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
+
+// -------------------- Task 2: Goi phan hoi --------------------
+void Task_UART_Feedback(void *pvParameters) {
+    char recvBuffer[MAX];
+ //   USART1_SendString("Task_UART_Feedback started\r\n");
+
+    while (1) {
+        if (xQueueReceive(xQueueCmd, recvBuffer, portMAX_DELAY) == pdTRUE) {
+            USART1_SendString("Feedback: ");
+            USART1_SendString(recvBuffer);
+            USART1_SendString("\n");
+            xQueueSend(xQueueFeedback, recvBuffer, 0);
+        }
+    }
+}
+
+// -------------------- Task 3: Ki?m tra l?nh --------------------
+void Task_Check_Command(void *pvParameters) {
+    char cmdBuffer[MAX];
+    Command_t cmd;
+  //  USART1_SendString("Task_Check_Command started\r\n");
+
+    while (1) {
+        if (xQueueReceive(xQueueFeedback, cmdBuffer, portMAX_DELAY) == pdTRUE) {
+            if (strcmp(cmdBuffer, "ON") == 0) cmd = CMD_ON;
+            else if (strcmp(cmdBuffer, "OFF") == 0) cmd = CMD_OFF;
+            else if (strcmp(cmdBuffer, "TOGGLE") == 0) cmd = CMD_TOGGLE;
+            else cmd = CMD_NONE;
+
+            if (cmd != CMD_NONE) {
+                xQueueSend(xQueueAction, &cmd, 0);
+            }
+        }
+    }
+}
+
+// -------------------- Task 4: Ði?u khi?n GPIO --------------------
+void Task_GPIO_Control(void *pvParameters) {
+    Command_t cmd;
+  //  USART1_SendString("Task_GPIO_Control started\r\n");
+
+    while (1) {
+        if (xQueueReceive(xQueueAction, &cmd, portMAX_DELAY) == pdTRUE) {
+            switch (cmd) {
+                case CMD_ON:    GPIO_ResetBits(GPIOA, GPIO_Pin_0); break;
+                case CMD_OFF:   GPIO_SetBits(GPIOA, GPIO_Pin_0); break;
+                case CMD_TOGGLE: GPIOA->ODR ^= GPIO_Pin_0; break;
+                default: break;
+            }
+        //    USART1_SendString("GPIO action done\r\n");
+        }
+    }
+}
+
+int main(void) {
+    SystemInit();
+    PIN_MODE_UART1();
+    Config_LED();
+
+    // Tao queue
+    xQueueCmd = xQueueCreate(3, MAX);
+    xQueueFeedback = xQueueCreate(3, MAX);
+    xQueueAction = xQueueCreate(3, sizeof(Command_t));
+
+    USART1_SendString("System started\r\n");
+
+    // Tao task
+    xTaskCreate(Task_UART_Receive, "UART_RX", 256, NULL, 2, NULL);
+    xTaskCreate(Task_UART_Feedback, "UART_FB", 256, NULL, 2, NULL);
+    xTaskCreate(Task_Check_Command, "CMD_Check", 256, NULL, 2, NULL);
+    xTaskCreate(Task_GPIO_Control, "GPIO_CTRL", 256, NULL, 2, NULL);
+
+    vTaskStartScheduler();
+
+    while (1);
 }
